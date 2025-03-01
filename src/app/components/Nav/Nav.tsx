17c0a5c6e6e56800b0faa8a4cd330441d1672d55
Kirill Senchuk <kimerland.project@gmail.com>
2025-03-01 23:58:22 +0300

    Nav changes

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -11,43 +11,47 @@ const Nav = () => {
   const toogleDropDown = () => {
     setIsDropdownOpen((prev) => !prev);
   };
+
   return (
-    <PageBlockWrapper backgroundColor={Colors.backgroundColorNav}>
-      <main className="main">
-        <div className="wrapper">
-          <div className="logo-weather">
-            <p className="text-weather">Weather</p>
-          </div>
+    <PageBlockWrapper
+    // backgroundColor={
+    //   !isDropdownOpen ? Colors.backgroundColorNav : Colors.backgroundColorMain
+    // }
+    >
+      {!isDropdownOpen ? (
+        <main className="main">
+          <div className="wrapper">
+            <div className="logo-weather">
+              <p className="text-weather">Weather</p>
+            </div>
 
-          <div className="rightSection">
-            <div className="drop-menu">
-              <div className="input-container">
-                <input
-                  onBlur={() => setIsDropdownOpen(false)}
-                  onClick={toogleDropDown}
-                  placeholder="Enter a city"
-                  type="text"
-                  className="input-weather"
-                />
-                <button className="search-icon">
-                  <LoopAssets width="30" height="30" />
-                </button>
-              </div>
-
-              {isDropdownOpen && (
-                <div className="dropdown-content">
-                  <div className="drop-inner">
-                    <div className="logo-weather">
-                      <p className="text-weather">Weather</p>
-                    </div>
-                    {/* fix this */}
-                  </div>
+            <div className="rightSection">
+              <div className="drop-menu">
+                <div className="input-container">
+                  <input
+                    onBlur={() => setIsDropdownOpen(false)}
+                    onClick={toogleDropDown}
+                    placeholder="Enter a city"
+                    type="text"
+                    className="input-weather"
+                  />
+                  <button className="search-icon">
+                    <LoopAssets width="30" height="30" />
+                  </button>
                 </div>
-              )}
+              </div>
+            </div>
+          </div>
+        </main>
+      ) : (
+        <div className="main-menu">
+          <div className="wrapper-menu">
+            <div className="logo-weather">
+              <p className="text-weather">Weather</p>
             </div>
           </div>
         </div>
-      </main>
+      )}
     </PageBlockWrapper>
   );
 };
